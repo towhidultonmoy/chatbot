aaf4559a480dca4adc396669901b3f9fff77a3b1
Towhidul Tonmoy <55044336+towhidultonmoy@users.noreply.github.com>
2025-07-17 16:14:36 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    https: {
-      key: fs.readFileSync('./ssl/key.pem'),
-      cert: fs.readFileSync('./ssl/cert.pem')
-    },
     proxy: {
       '/text': {
         target: 'http://localhost:5000',
