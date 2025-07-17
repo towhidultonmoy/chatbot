10065ed22e3a745be5972904eb16a9d473c960ac
Towhidul Tonmoy <55044336+towhidultonmoy@users.noreply.github.com>
2025-07-17 18:09:17 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     allowedHosts: [
       'localhost',
       '127.0.0.1',
-      '441cf1a83eb9.ngrok-free.app'
+      '9165d7cdbe2d.ngrok-free.app'
     ]
     // Or, for dev, you can do:
     // allowedHosts: 'all'
